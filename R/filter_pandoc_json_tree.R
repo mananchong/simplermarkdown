10b583d670c3b338398c2f0368a90b17d6fad0fd
mananchong <mananchong@gmail.com>
2022-06-14 13:42:43 +0800

    modify `fromJSON` 's args

--- R/filter_pandoc_json_tree.R
+++ R/filter_pandoc_json_tree.R
@@ -1,38 +1,43 @@
 
 #' @importFrom jsonlite fromJSON
-#' 
+#'
 filter_pandoc_json_tree <- function(con) {
   if (missing(con)) {
     con <- file("stdin")
     input <- readLines(con, warn = FALSE)
     close(con)
-    dta <- jsonlite::fromJSON(input, simplify = FALSE)
+    dta <- jsonlite::fromJSON(input, simplifyVector = FALSE)
   } else {
-    dta <- jsonlite::fromJSON(file = con, simplify = FALSE)
+    dta <- jsonlite::fromJSON(file = con, simplifyVector = FALSE)
   }
   # Extra step needed to get correct output; when meta is empty make sure
   # it is a named list; otherwire toJSON will generate 'meta: []' instead
   # of 'meta: {}' what pandoc expects
-  if (is.list(dta$meta) && length(dta$meta) == 0) 
+  if (is.list(dta$meta) && length(dta$meta) == 0) {
     names(dta$meta) <- character(0)
+  }
   default_fun <- "output_eval"
-  verbosity   <- 1
+  verbosity <- 1
   # Go over all of the blocks in the tree; check if they contain R code and
   # evaluate the code
-  dta$blocks <- parse_blocks(dta$blocks, default_fun = default_fun, 
-    verbosity = verbosity)
+  dta$blocks <- parse_blocks(dta$blocks,
+    default_fun = default_fun,
+    verbosity = verbosity
+  )
   dta
 }
 
 
-parse_blocks <- function(blocks, verbosity = 1, default_fun = "output_eval", 
-    eval_block = evaluate_code_block, eval_inline = evaluate_inline_code) {
+parse_blocks <- function(blocks, verbosity = 1, default_fun = "output_eval",
+                         eval_block = evaluate_code_block, eval_inline = evaluate_inline_code) {
   # The following type can all be handled the same way
   basic_types <- c("Emph", "Para", "Plain", "BlockQuote")
   # In the following types of content we will not look for code blocks and these
   # will nog be processed further
-  types_to_ignore <- c("Str", "Space", "Str", "Strong", "Link", "Image", 
-    "Table", "SoftBreak")
+  types_to_ignore <- c(
+    "Str", "Space", "Str", "Strong", "Link", "Image",
+    "Table", "SoftBreak"
+  )
   # Loop over the blocks and depending on the type process further; when we have
   # a block of R-code we will evaluate the code within it
   for (i in seq_along(blocks)) {
@@ -40,31 +45,46 @@ parse_blocks <- function(blocks, verbosity = 1, default_fun = "output_eval",
     if (block$t == "Code") {
       block <- eval_inline(block, verbosity = verbosity)
     } else if (block$t == "CodeBlock") {
-      block <- eval_block(block, verbosity = verbosity, 
-        default_fun = default_fun)
+      block <- eval_block(block,
+        verbosity = verbosity,
+        default_fun = default_fun
+      )
     } else if (block$t == "Header") {
-      block$c[[3]] <- parse_blocks(block$c[[3]], verbosity, default_fun,
-        eval_block, eval_inline)
+      block$c[[3]] <- parse_blocks(
+        block$c[[3]], verbosity, default_fun,
+        eval_block, eval_inline
+      )
     } else if (block$t == "BulletList") {
-      for (j in seq_along(block$c)) 
-        block$c[[j]] <- parse_blocks(block$c[[j]], verbosity, default_fun,
-        eval_block, eval_inline)
+      for (j in seq_along(block$c)) {
+        block$c[[j]] <- parse_blocks(
+          block$c[[j]], verbosity, default_fun,
+          eval_block, eval_inline
+        )
+      }
     } else if (block$t == "OrderedList") {
-      for (j in seq_along(block$c[[2]])) 
-        block$c[[2]][[j]] <- parse_blocks(block$c[[2]][[j]], verbosity, default_fun,
-        eval_block, eval_inline)
+      for (j in seq_along(block$c[[2]])) {
+        block$c[[2]][[j]] <- parse_blocks(
+          block$c[[2]][[j]], verbosity, default_fun,
+          eval_block, eval_inline
+        )
+      }
     } else if (block$t == "Div") {
-      block$c[[2]] <- parse_blocks(block$c[[2]], verbosity, default_fun,
-        eval_block, eval_inline)
+      block$c[[2]] <- parse_blocks(
+        block$c[[2]], verbosity, default_fun,
+        eval_block, eval_inline
+      )
     } else if (block$t %in% basic_types) {
-      block$c <- parse_blocks(block$c, verbosity, default_fun,
-        eval_block, eval_inline)
+      block$c <- parse_blocks(
+        block$c, verbosity, default_fun,
+        eval_block, eval_inline
+      )
     } else if (block$t %in% types_to_ignore) {
       # do nothing
     } else {
       # also do nothing but report
-      if (verbosity > 1)
-        warning("Ignoring unsupported block type '", block$t, "'.");
+      if (verbosity > 1) {
+        warning("Ignoring unsupported block type '", block$t, "'.")
+      }
     }
     blocks[[i]] <- block
   }
@@ -83,32 +103,35 @@ evaluate_inline_code <- function(block, verbosity = 1) {
 
 evaluate_code_block <- function(block, default_fun = "output_eval", verbosity = 1) {
   b <- get_block(block)
-  if (is.null(b)) return(block)
+  if (is.null(b)) {
+    return(block)
+  }
 
   if (b$language == "R") {
     id <- if (b$id == "") "<unlabeled code block>" else b$id
     if (verbosity > 0) message("Evaluating code in block '", id, "'.")
     if (exists("fun", b$arguments)) {
-      fun <- b$arguments$fun 
+      fun <- b$arguments$fun
       b$arguments$fun <- NULL
     } else {
       fun <- default_fun
     }
     res <- do.call(fun, c(
-      list(code = b$code, id = b$id, language = b$language), b$arguments))
+      list(code = b$code, id = b$id, language = b$language), b$arguments
+    ))
     block <- if (is.character(res)) raw_block(res) else res
   } else {
     # Other languages than R; only run if output fun is given
     if (exists("fun", b$arguments)) {
       id <- if (b$id == "") "<unlabeled code block>" else b$id
       if (verbosity > 0) message("Evaluating ", b$language, " code in block '", id, "'.")
-      fun <- b$arguments$fun 
+      fun <- b$arguments$fun
       b$arguments$fun <- NULL
       res <- do.call(fun, c(
-        list(code = b$code, id = b$id, language = b$language), b$arguments))
+        list(code = b$code, id = b$id, language = b$language), b$arguments
+      ))
       block <- if (is.character(res)) raw_block(res) else res
     }
   }
   block
 }
-
--- R/mdtangle.R
+++ R/mdtangle.R
@@ -2,36 +2,36 @@
 #'
 #' @param fn filename of the markdown file (should use pandoc markdown).
 #' @param ofn name of the resulting R-script
-#' @param extra_arguments extra arguments passed on to pandoc. Should be a length 1 
+#' @param extra_arguments extra arguments passed on to pandoc. Should be a length 1
 #'  character vector.
-#' @param cmd command used to run pandoc. See details. 
+#' @param cmd command used to run pandoc. See details.
 #' @param ... ignored
 #'
 #' @details
 #' \code{mdtangle} calls pandoc. Pandoc will parse the markdown document and
-#' write the parsed file to temporary file. This file is read by 
+#' write the parsed file to temporary file. This file is read by
 #' \code{mdtangle} and the code is extracted from it and written to \code{ofn}.
 #'
 #' Using the \code{cmd} argument the exact command used to run pandoc can be
-#' modified. It is passed on to \code{\link{sprintf}} and uses positional 
+#' modified. It is passed on to \code{\link{sprintf}} and uses positional
 #' arguments: (1) name of the input file, (2) location of the temporary file
-#' to which the parsed document is written, (3) the value of 
+#' to which the parsed document is written, (3) the value of
 #' \code{extra_arguments}.
 #'
-#' @return 
-#' Returns the filename of the generated file. 
+#' @return
+#' Returns the filename of the generated file.
 #'
 #' @export
-#' 
-mdtangle <- function(fn, ofn = file_subs_ext(basename(fn), ".R"), 
-    extra_arguments = "", cmd = 'pandoc %3$s -s "%1$s" -t json -o "%2$s"', ...) {
+#'
+mdtangle <- function(fn, ofn = file_subs_ext(basename(fn), ".R"),
+                     extra_arguments = "", cmd = 'pandoc %3$s -s "%1$s" -t json -o "%2$s"', ...) {
   tmp_ofn <- tempfile(fileext = ".json")
   on.exit(file.remove(tmp_ofn))
   cmd <- sprintf(cmd, fn, tmp_ofn, extra_arguments)
   system(cmd)
-  dta <- jsonlite::fromJSON(file = tmp_ofn, simplify = FALSE)
+  dta <- jsonlite::fromJSON(file = tmp_ofn, simplifyVector = FALSE)
   default_fun <- "output_eval"
-  verbosity   <- 1
+  verbosity <- 1
   code <- character(0)
   gather_code <- function(block, default_fun = "eval", verbosity = 1) {
     b <- get_block(block)
@@ -42,14 +42,15 @@ mdtangle <- function(fn, ofn = file_subs_ext(basename(fn), ".R"),
     }
     block
   }
-  ignore_inline <- function(block, verbosity = 1)  {
+  ignore_inline <- function(block, verbosity = 1) {
     block
   }
   # Go over all of the blocks in the tree; check if they contain R code and
   # evaluate the code
-  dta$blocks <- parse_blocks(dta$blocks, default_fun = default_fun, 
-    verbosity = verbosity, eval_block = gather_code, eval_inline = ignore_inline)
+  dta$blocks <- parse_blocks(dta$blocks,
+    default_fun = default_fun,
+    verbosity = verbosity, eval_block = gather_code, eval_inline = ignore_inline
+  )
   writeLines(code, con = ofn)
   invisible(ofn)
 }
-
